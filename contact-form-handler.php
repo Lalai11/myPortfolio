9b65722b9345a92ecaf862aca8c60206f2566b36
Laize Ferraz <laizeferraz@gmail.com>
2020-05-05 22:51:02 +1000

    Projects descriptions, contact form connection PHP

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,43 +1,62 @@
 <?php
-$name = $_POST['txtName'];
-$emailAddress = $_POST['txtEmail'];
-$message = $_POST['txtComment'];
-$errors = '';
-$myemail = 'laizeferraz@gmail.com';
+$errors = "";
 
-if(empty($name) ||
-   empty($emailAddress)) {
-     echo "Error: Name and Email are required.";
-   }
+$myemail = "laizeferraz@gmail.com"; 
 
-   if(!preg_match(
-    "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", $emailAddress)) {
-      $errors .= "\n Error: Invalid email address";
-    }
+if(empty($_POST["txtName"]) ||
 
-    if(empty($errors)) {
-      $to = $myemail;
-      $emailSubject = "Portfolio contact form submission: $name";
-      $emailBody = "You have received a new message from your Portfolio website. Message details: \n Name: $name \n Email: $emailAddress \n Message \n $message";
-      
-      $headers = "From: $myemail\n";
-      $headers .= "Reply-to: $emailAddress";
+empty($_POST["txtEmail"]) ||
 
-      mail($to, $emailSubject, $emailBody, $headers);
-      header('Location: contact-form-thank-you.html');
-    }
-    ?>
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Contact fort handler</title>
-    </head>
-    <body>
-      <!-- This page will display if there is a error filling up the contact form. -->
-      <?php
-        echo nl2br($errors);
-        ?>
-    </body>
-    </html>
+empty($_POST["txtComment"]))
+
+{
+
+$errors .= "\n Error: all fields are required";
+
+}
+
+$name = $_POST["txtName"]; 
+
+$email_address = $_POST["txtEmail"];
+
+$message = $_POST["txtComment"];
+
+if (!preg_match(
+
+"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", $email_address))
+
+{
+
+$errors .= "\n Error: Invalid email address";
+
+}
+
+if( empty($errors))
+
+{
+
+$to = $myemail;
+
+$email_subject = "Contact form submission: $name";
+
+$email_body = "You have received a new message. ".
+
+" Here are the details:\n Name: $name \n ".
+
+"Email: $email_address\n Message \n $message";
+
+$headers = "From: $myemail\n";
+
+$headers .= "Reply-To: $email_address";
+
+mail($to,$email_subject,$email_body,$headers);
+
+//redirect to the ‘thank you’ page
+
+header("Location: contact-form-thank-you.html");
+
+}
+
+?>
+
+   
